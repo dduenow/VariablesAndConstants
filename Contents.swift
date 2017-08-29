cd31ab0010f44feb3e6d1f259239e09ab54a6c01
David Duenow <dwdb79@mail.missouri.edu>
2017-08-29 12:41:23 -0500

    Uploaded Mini Challenge Playground

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var productName : String = "iPhone"
+
+var price : Double = 749.95
+
+var amount : Int = 1
+
+var shouldBuy : Bool = true
+
+let e : Double = 2.7183
+
+let spanishHello : String = "Hola"
+
+if shouldBuy == true {
+    print("Purchased")
+} else {
+    print("No Thanks")
+}
